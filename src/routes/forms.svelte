eb85d71e56d881ae6ac71dbbc99f0dcc7b4b2d02
Brandon Shields <brandon.shields@alaskaair.com>
2022-08-02 11:53:04 -0700

    feat: add page that shows our default form options

---
+++ src/routes/forms.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script>
+import DefaultFormElements from "$lib/content/DefaultFormElements.svelte"; 
+</script>
+
+<svelte:head>
+  <title>Grid: Demo of some of the ways to layout rows</title>
+</svelte:head>
+
+
+<div class="mainContent">
+  <div class="row"><DefaultFormElements /></div>
+</div>
+
+<style>
+	.mainContent {
+    padding: var(--padding);
+    display: block;
+}
+.row {
+  margin-bottom: 3rem;
+}
+
+</style>
