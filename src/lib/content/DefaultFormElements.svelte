ae50a733b0fbe08ff845673efe1c6015dffe5400
Brandon Shields <brandon.shields@alaskaair.com>
2022-08-02 11:53:04 -0700

    feat: add page that shows our default form options

---
+++ src/lib/content/DefaultFormElements.svelte
@@ -0,0 +1,77 @@
+<auro-header level="3" margin="top" size="none" display="500">These are our current `bordered` defalts for all of our form elements</auro-header>
+<div class="grid">
+  <div class="grid-item ">
+    <auro-header level="4" margin="bottom" size="none" display="200">Auro Input</auro-header>
+    <auro-input bordered></auro-input>
+    <auro-header level="4" margin="bottom" size="none" display="200">Auro Select</auro-header>
+    <auro-select>
+      <auro-menu>
+        <auro-menuoption value="stops">Stops</auro-menuoption>
+        <auro-menuoption value="price">Price</auro-menuoption>
+        <auro-menuoption value="duration">Duration</auro-menuoption>
+        <auro-menuoption value="departure">Departure</auro-menuoption>
+        <auro-menuoption value="arrival">Arrival</auro-menuoption>
+        <auro-menuoption value="prefer alaska">Prefer Alaska</auro-menuoption>
+      </auro-menu>
+    </auro-select>
+    <auro-header level="4" margin="bottom" size="none" display="200">Auro Combobox</auro-header>
+    <auro-combobox>
+      <auro-menu>
+        <auro-menuoption value="Apples" id="option-0">Apples</auro-menuoption>
+        <auro-menuoption value="Oranges" id="option-1">Oranges</auro-menuoption>
+        <auro-menuoption value="Peaches" id="option-2">Peaches</auro-menuoption>
+        <auro-menuoption value="Grapes" id="option-3">Grapes</auro-menuoption>
+        <auro-menuoption value="Cherries" id="option-4">Cherries</auro-menuoption>
+        <auro-menuoption static nomatch>No matching option</auro-menuoption>
+      </auro-menu>
+    </auro-combobox>
+    <auro-header level="4" margin="bottom" size="none" display="200">Auro Datepicker</auro-header>
+    <auro-datepicker></auro-datepicker>
+    </div>
+</div>
+<auro-header level="3" margin="top" size="none" display="500">These are our current defalts for all of our form elements with a background</auro-header>
+
+<div class="grid">
+  <div class="grid-item background-color">
+    <div class="avatar-details">
+      <auro-header level="4" margin="bottom" size="none" display="200">Auro Input</auro-header>
+      <auro-input bordered></auro-input>
+      <auro-header level="4" margin="bottom" size="none" display="200">Auro Select</auro-header>
+      <auro-select>
+        <auro-menu>
+          <auro-menuoption value="stops">Stops</auro-menuoption>
+          <auro-menuoption value="price">Price</auro-menuoption>
+          <auro-menuoption value="duration">Duration</auro-menuoption>
+          <auro-menuoption value="departure">Departure</auro-menuoption>
+          <auro-menuoption value="arrival">Arrival</auro-menuoption>
+          <auro-menuoption value="prefer alaska">Prefer Alaska</auro-menuoption>
+        </auro-menu>
+      </auro-select>
+      <auro-header level="4" margin="bottom" size="none" display="200">Auro Combobox</auro-header>
+      <auro-combobox>
+        <auro-menu>
+          <auro-menuoption value="Apples" id="option-0">Apples</auro-menuoption>
+          <auro-menuoption value="Oranges" id="option-1">Oranges</auro-menuoption>
+          <auro-menuoption value="Peaches" id="option-2">Peaches</auro-menuoption>
+          <auro-menuoption value="Grapes" id="option-3">Grapes</auro-menuoption>
+          <auro-menuoption value="Cherries" id="option-4">Cherries</auro-menuoption>
+          <auro-menuoption static nomatch>No matching option</auro-menuoption>
+        </auro-menu>
+      </auro-combobox>
+      <auro-header level="4" margin="bottom" size="none" display="200">Auro Datepicker</auro-header>
+      <auro-datepicker></auro-datepicker>
+    </div>
+  </div>
+</div>
+
+
+
+<style>
+  .grid {
+  margin-bottom: 1rem;
+}
+  .background-color {
+    background-color: #ccc;
+    padding: 1rem;
+  }
+</style>
