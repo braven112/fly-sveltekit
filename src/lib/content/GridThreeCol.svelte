cb624f8e5aa81d83ca4b495c93745d0ffddc7254
Brandon Shields <brandon.shields@alaskaair.com>
2022-08-08 16:46:35 -0700

    feat: add updated guidelines for 3 column layouts

--- src/lib/content/GridThreeCol.svelte
+++ src/lib/content/GridThreeCol.svelte
@@ -28,6 +28,66 @@
     </div>
   </div>
 </div>
+<auro-header level="3" margin="top" size="none" display="500">3 Column Layout - Squish</auro-header>
+<p>This row will stay on a row on mobile</p>
+<div class="grid">
+  <div class="grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+
+  <div class="grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SEA"></auro-avatar>
+        <div class="avatar-cityname">Seattle, WA</div>
+        <div class="avatar-airport fineprint">Seattle/Tacoma Intl.</div>
+        <div class="avatar-airport fineprint">SEA</div>
+    </div>
+  </div>
+</div>
+
+<auro-header level="3" margin="top" size="none" display="500">3 Column Layout - 2x1 aka (`col-6 col-xs-4`, `col-6 col-xs-4`, `col-12 col-xs-4`)</auro-header>
+<p>This row will collapse to 2 column on mobile with the 3rd column taking up with full width of the second row on mobile.  The implementor can decide what column takes the whole width, either the first item or the last item.</p>
+<div class="grid">
+  <div class="col-6 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-6 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+
+  <div class="col-12 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SEA"></auro-avatar>
+        <div class="avatar-cityname">Seattle, WA</div>
+        <div class="avatar-airport fineprint">Seattle/Tacoma Intl.</div>
+        <div class="avatar-airport fineprint">SEA</div>
+    </div>
+  </div>
+</div>
+
 <auro-header level="3" margin="top" size="none" display="500">3 Column Layout - Scroll</auro-header>
 <p>This row will scroll on mobile</p>
 <div class="grid grid-scroll-3">
--- src/lib/content/GridTwoCol.svelte
+++ src/lib/content/GridTwoCol.svelte
@@ -39,6 +39,27 @@
   </div>
 </div>
 
+<auro-header level="3" margin="top" size="none" display="500">2 Column Layout - Page Layout</auro-header>
+<p>The 2 column layout is super powerful and can be used to create full page layouts where one column houses a side nav and the rest of the space allows for content to stack.</p>
+<div class="grid">
+  <div class="col-12 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-8 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
 <auro-header level="3" margin="top" size="none" display="500">2 Column Layout - Scroll</auro-header>
 <p>We can also set it to scroll on mobile</p>
 <div class="grid grid-scroll-2">
@@ -51,6 +72,27 @@
     </div>
   </div>
   <div class="grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<auro-header level="3" margin="top" size="none" display="500">2 Column Layout - col-8 col-4</auro-header>
+<p>We can also set it to scroll on mobile</p>
+<div class="grid">
+  <div class="col-8 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-4 grid-item">
     <div class="avatar-details">
         <auro-avatar md code="SBP"></auro-avatar>
         <div class="avatar-cityname">San Luis Obispo, CA</div>
