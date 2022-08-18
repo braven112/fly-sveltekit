d41a052452766dba64cc01c295ba3829e4167c26
Brandon Shields <brandon.shields@alaskaair.com>
2022-08-18 16:01:03 -0700

    feat: new grid max size

--- src/lib/content/GridTwoCol.svelte
+++ src/lib/content/GridTwoCol.svelte
@@ -38,20 +38,113 @@
     </div>
   </div>
 </div>
-
-<auro-header level="3" margin="top" size="none" display="500">2 Column Layout - Page Layout</auro-header>
-<p>The 2 column layout is super powerful and can be used to create full page layouts where one column houses a side nav and the rest of the space allows for content to stack.</p>
-<p>For this scenario do we want to use our default grid system, or should we create a custom grid for this scenerio where we set a specific size for the "side nav" container and allow the right side to scale?  In this example both columns scale to size but we can set a min size for the left column so it will always house the side nav if we want.</p>
-<div class="grid-sidenav">
-  <div class="grid-item">
+<auro-header level="3" margin="top" size="none" display="500">2 Column Layout - Asymmetrical options</auro-header>
+<p>With these options we can set each column to align with the 12 column grid.  Broken down to 9/3, 8/4, 7/5.</p>
+<div class="grid">
+  <div class="col-12 col-xs-3 grid-item">
     <div class="avatar-details">
         <auro-avatar md code="AUS"></auro-avatar>
-        <div class="avatar-cityname">Side nav</div>
+        <div class="avatar-cityname">Austin, TX</div>
         <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
         <div class="avatar-airport fineprint">AUS</div>
     </div>
   </div>
-  <div class="grid-item">
+  <div class="col-12 col-xs-9 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<div class="grid">
+  <div class="col-12 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-8 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<div class="grid">
+  <div class="col-12 col-xs-5 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-7 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<div class="grid">
+  <div class="col-12 col-xs-9 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-3 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<div class="grid">
+  <div class="col-12 col-xs-8 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-4 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="SBP"></auro-avatar>
+        <div class="avatar-cityname">San Luis Obispo, CA</div>
+        <div class="avatar-airport fineprint">San Luis Obispo</div>
+        <div class="avatar-airport fineprint">SBP</div>
+    </div>
+  </div>
+</div>
+
+<div class="grid">
+  <div class="col-12 col-xs-7 grid-item">
+    <div class="avatar-details">
+        <auro-avatar md code="AUS"></auro-avatar>
+        <div class="avatar-cityname">Austin, TX</div>
+        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
+        <div class="avatar-airport fineprint">AUS</div>
+    </div>
+  </div>
+  <div class="col-12 col-xs-5 grid-item">
     <div class="avatar-details">
         <auro-avatar md code="SBP"></auro-avatar>
         <div class="avatar-cityname">San Luis Obispo, CA</div>
@@ -73,27 +166,6 @@
     </div>
   </div>
   <div class="grid-item">
-    <div class="avatar-details">
-        <auro-avatar md code="SBP"></auro-avatar>
-        <div class="avatar-cityname">San Luis Obispo, CA</div>
-        <div class="avatar-airport fineprint">San Luis Obispo</div>
-        <div class="avatar-airport fineprint">SBP</div>
-    </div>
-  </div>
-</div>
-
-<auro-header level="3" margin="top" size="none" display="500">2 Column Layout - col-8 col-4</auro-header>
-<p>We can also set it to scroll on mobile</p>
-<div class="grid">
-  <div class="col-8 grid-item">
-    <div class="avatar-details">
-        <auro-avatar md code="AUS"></auro-avatar>
-        <div class="avatar-cityname">Austin, TX</div>
-        <div class="avatar-airport fineprint">Austin/Bergstrom Intl.</div>
-        <div class="avatar-airport fineprint">AUS</div>
-    </div>
-  </div>
-  <div class="col-4 grid-item">
     <div class="avatar-details">
         <auro-avatar md code="SBP"></auro-avatar>
         <div class="avatar-cityname">San Luis Obispo, CA</div>
