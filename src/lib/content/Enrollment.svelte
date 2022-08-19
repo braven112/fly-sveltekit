6d7ad949204259e76ca8f1dc3fe99aed6919d840
Brandon Shields <brandon.shields@alaskaair.com>
2022-08-18 21:40:53 -0700

    chore: fix spacing

--- src/lib/content/Enrollment.svelte
+++ src/lib/content/Enrollment.svelte
@@ -1,5 +1,5 @@
 <div class="container alertContainer">
-    <auro-alert type="information">Travel Open Enrollment for <auro-datetime type="year"> is between December 1 - December 15. During this time, you will be able to verify student
+    <auro-alert type="information">Travel Open Enrollment for <auro-datetime type="year">&nbsp; is between December 1 - December 15. During this time, you will be able to verify student
         status, swap parents for step-parents, and make changes to your primary traveler for the upcoming year (if you are an active employee).</auro-alert>
 </div>
 <style>
