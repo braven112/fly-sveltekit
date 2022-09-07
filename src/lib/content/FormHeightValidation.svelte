dea2be842168a3557344672969ba42b7c70ba065
Brandon Shields <brandon.shields@alaskaair.com>
2022-09-06 21:04:11 -0700

    feat: fill out form height examples page

--- src/lib/content/FormHeightValidation.svelte
+++ src/lib/content/FormHeightValidation.svelte
@@ -1,4 +1,5 @@
-<auro-header level="3" margin="top" size="none" display="500">Default form inputs - No properties used in this example</auro-header>
+<auro-header level="3" margin="bottom" size="none" display="500">Default form inputs</auro-header>
+<auro-header level="5" margin="top" size="none" display="300">No properties used in this example</auro-header>
 <div class="grid grid--scroll-3">
   <div class="grid-item">
     <auro-combobox>
@@ -30,11 +31,13 @@
   </div>
 </div>
 
-<auro-header level="3" margin="top" size="none" display="500">Form inputs - Label and help text is included in these examples</auro-header>
+<auro-header level="3" margin="bottom" size="none" display="500">Auro Form inputs</auro-header>
+<auro-header level="5" margin="top" size="none" display="300">Both label and help text</auro-header>
 <div class="grid grid--scroll-3">
   <div class="grid-item">
     <auro-combobox>
-        <span slot="label">Name</span>
+        <span slot="label">This is a label</span>
+        <span slot="helpText">This is help text</span>
       <auro-menu>
         <auro-menuoption value="Apples" id="option-0">Apples</auro-menuoption>
         <auro-menuoption value="Oranges" id="option-1">Oranges</auro-menuoption>
@@ -48,8 +51,8 @@
 
   <div class="grid-item">
     <auro-select>
-        <span slot="label">Please choose a preference</span>
-        <span slot="helpText">Preferences are maintained for future use</span>
+        <span slot="label">This is a label</span>
+        <span slot="helpText">This is help text</span>
         <auro-menu>
           <auro-menuoption value="stops">Stops</auro-menuoption>
           <auro-menuoption value="price">Price</auro-menuoption>
@@ -61,10 +64,83 @@
       </auro-select>
   </div>
   <div class="grid-item">     
-      <auro-input bordered required icon type="credit-card" id="demo14">
-        <slot slot="label">Card number</slot>
-        <slot slot="helptext">Valid credit card numbers must include 16 digits (15 for Amex).</slot>
+      <auro-input bordered>
+        <span slot="label">This is a label</span>
+        <span slot="helptext">This is help text</span>
       </auro-input>
+  </div>
+</div>
+<auro-header level="3" margin="bottom" size="none" display="500">Auro Form inputs</auro-header>
+<auro-header level="5" margin="top" size="none" display="300">No Labels but with help text</auro-header>
+<div class="grid grid--scroll-3">
+  <div class="grid-item">
+    <auro-combobox>
+      <span slot="helpText">This is help text</span>
+      <auro-menu>
+        <auro-menuoption value="Apples" id="option-0">Apples</auro-menuoption>
+        <auro-menuoption value="Oranges" id="option-1">Oranges</auro-menuoption>
+        <auro-menuoption value="Peaches" id="option-2">Peaches</auro-menuoption>
+        <auro-menuoption value="Grapes" id="option-3">Grapes</auro-menuoption>
+        <auro-menuoption value="Cherries" id="option-4">Cherries</auro-menuoption>
+        <auro-menuoption static nomatch>No matching option</auro-menuoption>
+      </auro-menu>
+    </auro-combobox>
+  </div>
+
+  <div class="grid-item">
+    <auro-select>
+        <span slot="helpText">This is help text</span>
+        <auro-menu>
+          <auro-menuoption value="stops">Stops</auro-menuoption>
+          <auro-menuoption value="price">Price</auro-menuoption>
+          <auro-menuoption value="duration">Duration</auro-menuoption>
+          <auro-menuoption value="departure">Departure</auro-menuoption>
+          <auro-menuoption value="arrival">Arrival</auro-menuoption>
+          <auro-menuoption value="prefer alaska">Prefer Alaska</auro-menuoption>
+        </auro-menu>
+      </auro-select>
+  </div>
+  <div class="grid-item">     
+    <auro-input bordered>
+      <span slot="helptext">This is help text</span>
+    </auro-input>
+  </div>
+</div>
+
+<auro-header level="3" margin="bottom" size="none" display="500">Auro Form inputs</auro-header>
+<auro-header level="5" margin="top" size="none" display="300">Label but no help text</auro-header>
+<div class="grid grid--scroll-3">
+  <div class="grid-item">
+    <auro-combobox>
+        <span slot="label">This is a label</span>
+      <auro-menu>
+        <auro-menuoption value="Apples" id="option-0">Apples</auro-menuoption>
+        <auro-menuoption value="Oranges" id="option-1">Oranges</auro-menuoption>
+        <auro-menuoption value="Peaches" id="option-2">Peaches</auro-menuoption>
+        <auro-menuoption value="Grapes" id="option-3">Grapes</auro-menuoption>
+        <auro-menuoption value="Cherries" id="option-4">Cherries</auro-menuoption>
+        <auro-menuoption static nomatch>No matching option</auro-menuoption>
+      </auro-menu>
+    </auro-combobox>
+  </div>
+
+  <div class="grid-item">
+    <auro-select>
+        <span slot="label">This is a label</span>
+        <auro-menu>
+          <auro-menuoption value="stops">Stops</auro-menuoption>
+          <auro-menuoption value="price">Price</auro-menuoption>
+          <auro-menuoption value="duration">Duration</auro-menuoption>
+          <auro-menuoption value="departure">Departure</auro-menuoption>
+          <auro-menuoption value="arrival">Arrival</auro-menuoption>
+          <auro-menuoption value="prefer alaska">Prefer Alaska</auro-menuoption>
+        </auro-menu>
+      </auro-select>
+  </div>
+  <div class="grid-item">     
+    <auro-input bordered>
+      <span slot="label">This is a label</span>
+    </auro-input>
   </div>
 </div>
 
