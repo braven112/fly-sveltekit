f7b5b94f50f499ddeaf047f8d26af634f81e32e2
Brandon Shields <braven112@gmail.com>
2022-10-20 16:01:47 -0700

    feat: radio test

---
+++ src/lib/content/Radio.svelte
@@ -0,0 +1,13 @@
+<auro-header level="3" margin="bottom" size="none" display="500">Default form inputs</auro-header>
+<auro-header level="5" margin="top" size="none" display="300">No properties used in this example</auro-header>
+<div class="grid grid--scroll-3">
+  <div class="form-groups">
+    <auro-radio id="radio1" label="Yes" name="radioDemo" value="yes"></auro-radio>
+  </div>
+</div>
+
+<style>
+  .grid {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/routes/radio.svelte
@@ -0,0 +1,43 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script>
+  import Radio from "$lib/content/Radio.svelte";
+</script>
+
+<svelte:head>
+  <title>Forms: All of our auro form elements on one page</title>
+</svelte:head>
+
+<div class="mainContent">
+  <auro-header>Auro Forms</auro-header>
+    <p>
+      This page is intended to show all of our form elements all on one page. This page is NOT intended to show how we should do things. Its intended to show how they CAN be
+      implemented. This way we can align on fallbacks if necessary and find any potential bugs. We'll continue to add more examples.
+    </p>
+  <div class="row from-bg">
+    <auro-header display="700">background</auro-header>
+    <Radio />
+  </div>
+  <div class="row defalt">
+    <auro-header display="700">No background</auro-header>
+    <Radio />
+  </div>
+</div>
+
+<style>
+  .mainContent {
+    padding: var(--padding);
+    display: block;
+  }
+  .row {
+    margin-bottom: 3rem;
+    padding: var(--padding);
+    border: 1px solid var(--auro-color-border-disabled-on-light);
+    border-radius: var(--auro-border-radius);
+  }
+  .from-bg {
+    background-color: #eee;
+  }
+</style>
