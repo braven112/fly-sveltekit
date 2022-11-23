372cbd436303db6a4ed6c4c441ed8c5fad30cc22
Brandon Shields <braven112@gmail.com>
2022-11-22 21:39:29 -0800

    fix: update svelte configs

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		}
+		},
+		edge: false,
+		external: [],
+		split: false,
 	}
 };
 
